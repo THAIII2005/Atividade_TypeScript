ba97227e1ac6ab7e43cc3a2c67e73daba39b2ac4
Thaís Vitória <102629334+THAIII2005@users.noreply.github.com>
2026-03-13 08:38:00 -0400

    Delete formatarNome.ts

--- formatarNome.ts
+++
@@ -1,9 +0,0 @@
-function formatarNome(nome: string, sobrenome?: string): string{
-   if(sobrenome){
-    return `Olá ${nome} ${sobrenome}!`;
-   } 
-   return `Olá, ${nome}!`;
-}
-
-console.log(formatarNome("Thais Vitória", "Dias Rosa"));
-
