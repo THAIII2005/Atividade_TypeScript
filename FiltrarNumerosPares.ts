fca900865029f60a585cb9ce2583dd9d64d3b592
Thaís Vitória <102629334+THAIII2005@users.noreply.github.com>
2026-03-13 08:38:33 -0400

    Delete FiltrarNumerosPares.ts

--- FiltrarNumerosPares.ts
+++
@@ -1,9 +0,0 @@
-function filtrarPares(numeros: number[]): number[] {
-    return numeros.filter((numero) => numero % 2 === 0);
-}
-
-const meusNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const resultadoPares = filtrarPares(meusNumeros);
-
-console.log("Os números originais:", meusNumeros);
-console.log("Apenas os pares:", resultadoPares);
