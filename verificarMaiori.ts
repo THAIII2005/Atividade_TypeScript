83529eee9617a6a4f5e3671155724283b197d72a
Thaís Vitória <102629334+THAIII2005@users.noreply.github.com>
2026-03-13 08:37:27 -0400

    Delete verificarMaiori.ts

--- verificarMaiori.ts
+++
@@ -1,9 +0,0 @@
-function verificarMaioridade(
-  idade: number
-): boolean {
-  return idade > 18;
-}
-
-console.log(verificarMaioridade(20)); 
-console.log(verificarMaioridade(18)); 
-console.log(verificarMaioridade(15)); 
